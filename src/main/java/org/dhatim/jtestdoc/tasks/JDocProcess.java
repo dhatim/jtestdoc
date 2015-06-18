209e7f83788581f029ad4f3ac154b922432d0f7c
Maxime Suret <msuret@dhatim.net>
2015-06-18 15:12:32 +0200

    code review

--- src/main/java/org/dhatim/jtestdoc/tasks/JDocProcess.java
+++ src/main/java/org/dhatim/jtestdoc/tasks/JDocProcess.java
@@ -13,82 +13,82 @@ import org.dhatim.jtestdoc.utilities.JTDAG;
 import org.dhatim.jtestdoc.utilities.MethodSet;
 import org.dhatim.jtestdoc.visitors.TestTagVisitor;
 
-import org.dhatim.jtestdoc.tasks.JDocProcess;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import java.io.IOException;
+import java.util.List;
 
-public class JDocProcess extends Task 
-{
-	ArrayList<File> files = new ArrayList<File>(); //This is the list with all the processed files
-	String destination; //This is the place where the generated documentation will be exported
-	public ArrayList<MethodDeclaration> allmymethods;
-	FileSet f = new FileSet();//This is the fileset with the files to be processed
-	boolean blocking;//This defines if the task should throw errors(blocking the task at the first one) or simply generate warnings(and create a doc file even if there are warnings)
-	
+public class JDocProcess extends Task {
 
-	public void execute() throws BuildException 
-	{
-		if(destination==null)//If the destination is null, set it to a default location documentation.html
-			this.destination = "documentation.html";
+    private ArrayList<File> files = new ArrayList<>(); //This is the list with all the processed files
+    private String destination; //This is the place where the generated documentation will be exported
+    private FileSet f = new FileSet();//This is the fileset with the files to be processed
+    private boolean blocking;//This defines if the task should throw errors(blocking the task at the first one) or simply generate warnings(and create a doc file even if there are warnings)
 
-		 //Parsing files
-		for(Resource mfi:f)//Looping through the list of of files
-		{
-			MethodSet m = new MethodSet(blocking);
-			allmymethods = new ArrayList<MethodDeclaration>();
-			
-			File d=new File();
-			FileInputStream in=null;
-			CompilationUnit cu = null;
-				
-			try {
-				in = new FileInputStream(mfi.toString());
-				cu = JavaParser.parse(in);
-			} catch (ParseException|FileNotFoundException e) {
-				e.printStackTrace();
-			}
-			
-			TestTagVisitor a = new TestTagVisitor();
-	        new MethodVisitor().visit(cu, this);
-	        a.setAllmymethods(allmymethods);
-			
-	        a.visit(cu,m);
-			d.setMethods(a.getMethods());
-			d.setName(mfi.getName());
-			if(!d.getMethods().isEmpty())
-				files.add(d);
-		}
+    @Override
+    public void execute() throws BuildException {
+        if (destination == null)//If the destination is null, set it to a default location documentation.html
+        {
+            this.destination = "documentation.html";
+        }
 
-		JTDAG doc = new JTDAG(files, destination); //Create a JTestDocumentation with the files
-		doc.export(); //Export it
-	
-	}
-	
-	public void addFileSet(FileSet f)
-	{
-		this.f = f;
-	}
-	public void setBlocking(String s)
-	{
-		blocking = Boolean.parseBoolean(s);
-	}
-	public String getDestination() {
-		return destination;
-	}
+        //Parsing files
+        for (Resource mfi : f)//Looping through the list of of files
+        {
+            MethodSet methodSet = new MethodSet(blocking);
+            List<MethodDeclaration> fileMethods = new ArrayList<>();
 
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-	
-	private static class MethodVisitor extends VoidVisitorAdapter<Object> {
+            File d = new File();
+            CompilationUnit cu = null;
 
-	        @Override
-	        public void visit(MethodDeclaration n, Object arg) {
-	        	JDocProcess s = (JDocProcess) arg;
-	        	s.allmymethods.add(n);
-	        }
-	}
+            try (FileInputStream in = new FileInputStream(mfi.toString())) {
+                cu = JavaParser.parse(in);
+            } catch (IOException | ParseException e) {
+                throw new BuildException(e);
+            }
+
+            new MethodVisitor().visit(cu, fileMethods);
+
+            TestTagVisitor testTagVisitor = new TestTagVisitor();
+            testTagVisitor.setAllmymethods(fileMethods);
+            testTagVisitor.visit(cu, methodSet);
+
+            d.setMethods(testTagVisitor.getMethods());
+            d.setName(mfi.getName());
+            if (!d.getMethods().isEmpty()) {
+                files.add(d);
+            }
+
+            JTDAG doc = new JTDAG(files, destination); //Create a JTestDocumentation with the files
+            doc.export(); //Export it
+
+        }
+    }
+
+    public void addFileSet(FileSet f) {
+        this.f = f;
+    }
+
+    public void setBlocking(String s) {
+        blocking = Boolean.parseBoolean(s);
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    private static class MethodVisitor extends VoidVisitorAdapter<List<MethodDeclaration>> {
+
+        @Override
+        public void visit(MethodDeclaration n, List<MethodDeclaration> methods) {
+            methods.add(n);
+        }
+    }
 }
