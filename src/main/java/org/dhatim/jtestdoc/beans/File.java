08d6b6ec6c73744abd5801bae4836a75b153c0c7
Maxime Suret <msuret@dhatim.net>
2015-06-18 15:25:55 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/java/org/dhatim/jtestdoc/beans/File.java
    	src/main/java/org/dhatim/jtestdoc/beans/Method.java

--- src/main/java/org/dhatim/jtestdoc/beans/File.java
+++ src/main/java/org/dhatim/jtestdoc/beans/File.java
@@ -1,12 +1,12 @@
 package org.dhatim.jtestdoc.beans;
 
 import java.util.ArrayList;
+import java.util.List;
 
-<<<<<<< HEAD
 public class File {
 
     private String name;
-    private ArrayList<Method> methods;
+    private List<Method> methods;
 
     public File() {
     }
@@ -25,42 +25,12 @@ public class File {
         this.name = name;
     }
 
-    public ArrayList<Method> getMethods() {
+    public List<Method> getMethods() {
         return methods;
     }
 
-    public void setMethods(ArrayList<Method> methods) {
+    public void setMethods(List<Method> methods) {
         this.methods = methods;
     }
 
-=======
-import org.dhatim.jtestdoc.beans.Method;
-
-
-public class File 
-{
-	private String name;
-	private ArrayList<Method> methods;
-	
-	public File(){}
-	public File(String name, ArrayList<Method> methods) {
-		super();
-		this.name = name;
-		this.methods = methods;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public ArrayList<Method> getMethods() {
-		return methods;
-	}
-	public void setMethods(ArrayList<Method> methods) {
-		this.methods = methods;
-	}
-	
-	
->>>>>>> origin/master
 }
--- src/main/java/org/dhatim/jtestdoc/beans/Method.java
+++ src/main/java/org/dhatim/jtestdoc/beans/Method.java
@@ -2,12 +2,11 @@ package org.dhatim.jtestdoc.beans;
 
 import java.util.List;
 
-<<<<<<< HEAD
 public class Method {
 
     private String name;
     private String initialState;
-    private ArrayList<XStep> steps;
+    private List<XStep> steps;
     private String description;
 
     public String getName() {
@@ -26,11 +25,11 @@ public class Method {
         this.initialState = initialState;
     }
 
-    public ArrayList<XStep> getSteps() {
+    public List<XStep> getSteps() {
         return steps;
     }
 
-    public void setSteps(ArrayList<XStep> steps) {
+    public void setSteps(List<XStep> steps) {
         this.steps = steps;
     }
 
@@ -42,41 +41,4 @@ public class Method {
         this.description = description;
     }
 
-=======
-import org.dhatim.jtestdoc.beans.XStep;
-
-public class Method 
-{
-	public String name;
-	public String initialState;
-	public List<XStep> steps;
-	public String description;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getInitialState() {
-		return initialState;
-	}
-	public void setInitialState(String initialState) {
-		this.initialState = initialState;
-	}
-	public List<XStep> getSteps() {
-		return steps;
-	}
-	public void setSteps(List<XStep> list) {
-		this.steps = list;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	
->>>>>>> origin/master
 }
