d8c5d12775a9d7c2e8fa2b54c96646c6adb436f1
Maxime Suret <msuret@dhatim.com>
2015-06-23 18:00:36 +0200

    fix "destination" attribute issue + upgrade plugin version
    the "destination" attribute was not taken into account.
    Furthermore, the json css and js files must be generated in the same
    folder as the html.
    The "destination" attribute is now a folder where all files are
    generated. the html file is always named "documentation.html"

--- src/main/java/org/dhatim/jtestdoc/utilities/JTDAG.java
+++ src/main/java/org/dhatim/jtestdoc/utilities/JTDAG.java
@@ -15,82 +15,81 @@ import java.util.List;
 /**
  * Java Test Documentation Automatically Generated is in charge of the
  * exportation of the documentation
- * 
+ *
  * @author Nathanaël Langlois
  * @version 1.0
  */
 public class JTDAG {
 
-	// All files that need to be processed
-	List<File> files;
+    // All files that need to be processed
+    private final List<File> files;
 
-	// Path of the final file
-	String destination;
+    // Path of the destination folder
+    private final String destination;
 
-	// Template files
-	private String template = "";
-	private String jsparser = "";
-	private String markdown = "";
+    // Template files
+    private final String template;
+    private final String jsparser;
+    private final String markdown;
 
-	/**
-	 * This constructor initializes the templates
-	 * 
-	 * @param files
-	 *            the files that were processed
-	 * @param destination
-	 *            where the user wants to put his documentation
-	 */
-	public JTDAG(ArrayList<File> files, String destination) {
-		this.files = files;
-		this.destination = destination;
+    /**
+     * This constructor initializes the templates
+     *
+     * @param files the files that were processed
+     * @param destination where the user wants to put his documentation
+     */
+    public JTDAG(ArrayList<File> files, String destination) {
+        this.files = files;
+        this.destination = destination;
 
-		try (Scanner templateScanner = new Scanner(JTDAG.class.getClassLoader()
-				.getResourceAsStream("template.html"), "UTF-8");
-				Scanner parserScanner = new Scanner(JTDAG.class
-						.getClassLoader().getResourceAsStream(
-								"jsparser.js.template"), "UTF-8");
-				Scanner markedScanner = new Scanner(JTDAG.class
-						.getClassLoader().getResourceAsStream("marked.js"),
-						"UTF-8");) {
-			while (templateScanner.hasNextLine())
-				template += templateScanner.nextLine() + "\n";
-			while (parserScanner.hasNextLine())
-				jsparser += parserScanner.nextLine() + "\n";
-			while (markedScanner.hasNextLine())
-				markdown += markedScanner.nextLine() + "\n";
-		}
+        try (
+                Scanner templateScanner = new Scanner(
+                        JTDAG.class.getClassLoader().getResourceAsStream("template.html"),
+                        "UTF-8")
+                    .useDelimiter("\\Z");
+                Scanner parserScanner = new Scanner(
+                        JTDAG.class.getClassLoader().getResourceAsStream("jsparser.js.template"),
+                        "UTF-8")
+                    .useDelimiter("\\Z");
+                Scanner markedScanner = new Scanner(
+                        JTDAG.class.getClassLoader().getResourceAsStream("marked.js"),
+                        "UTF-8")
+                    .useDelimiter("\\Z");) {
+            template = templateScanner.next();
+            jsparser = parserScanner.next();
+            markdown = markedScanner.next();          
+        }
 
-	}
+    }
 
-	/**
-	 * This method exports a documentation file from what was processed
-	 * 
-	 * @throws BuildException
-	 *             if the exportation doesn't work
-	 */
-	public void export() throws BuildException {
-		Gson gson = new Gson();
-		String json = "";
+    /**
+     * This method exports a documentation file from what was processed
+     *
+     * @throws BuildException if the exportation doesn't work
+     */
+    public void export() throws BuildException {
+        Gson gson = new Gson();
+        String json = "";
 
-		json += gson.toJson(files);
+        json += gson.toJson(files);
 
-		try {
-			try (FileWriter writer = new FileWriter("doc.json")) {
-				writer.write(json);
-			}
-			try (FileWriter writer0 = new FileWriter("doc.js")) {
-				writer0.write("var doc = JSON.parse('" + json + "'" + jsparser);
-			}
-			try (FileWriter writer2 = new FileWriter("documentation.html")) {
-				writer2.write(template);
-			}
-			try (FileWriter writer3 = new FileWriter("marked.js")) {
-				writer3.write(markdown);
-			}
-		} catch (IOException e) {
-			throw new BuildException(e);
-		}
+        try {
+            try (FileWriter writer = new FileWriter(new java.io.File(destination, "doc.json"))) {
+                writer.write(json);
+            }
+            try (FileWriter writer0 = new FileWriter(new java.io.File(destination, "doc.js"))) {
+                writer0.write("var doc = JSON.parse('" + json + "'" + jsparser);
+            }
+            try (FileWriter writer2 = new FileWriter(new java.io.File(destination, "documentation.html"))) {
+                writer2.write(template);
+            }
+            try (FileWriter writer3 = new FileWriter(new java.io.File(destination, "marked.js"))) {
+                writer3.write(markdown);
+            }
+        } catch (IOException e) {
+            throw new BuildException(e);
+        }
 
-	}
+    }
 
 }
